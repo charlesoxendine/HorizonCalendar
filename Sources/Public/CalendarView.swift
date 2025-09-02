36abc627ef70011e1f955fb99eb5661fe2f694ad
charlesoxendine <102759864+charlesoxendine@users.noreply.github.com>
2025-09-01 22:28:05 -0400

    trying to fix layout issue

--- Sources/Public/CalendarView.swift
+++ Sources/Public/CalendarView.swift
@@ -633,6 +633,8 @@ public final class CalendarView: UIView {
       backgroundColor = .white
     }
 
+    insetsLayoutMarginsFromSafeArea = false
+      
     // Must be the first subview so that `UINavigationController` can monitor its scroll position
     // and make navigation bars opaque on scroll.
     insertSubview(scrollView, at: 0)
